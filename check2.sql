ac252475b25ced10412926e0adc9b8fb4eedccee
veronikajnnd <y03372@yagami.co.jp>
2026-08-04 13:17:55 +0900

    Remove pager command from check2.sql
    Remove unnecessary command to disable pager in SQL script.

--- check2.sql
+++ check2.sql
@@ -1,5 +1,3 @@
-sed -i '1i \\pset pager off' check2.sql
-
 DROP FUNCTION IF EXISTS tmp_kana_to_romaji(text);
 DROP TABLE IF EXISTS tmp_manufacturer_normalized, tmp_katakana_romaji_map, tmp_manufacturer_romaji;
 
