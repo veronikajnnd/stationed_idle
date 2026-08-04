b25cb9b7d2ed46420affef7ed5261391622c2baa
verojnnd <y03372@yagami.co.jp>
2026-08-04 13:48:14 +0900

    Update check3.sql

--- check3.sql
+++ check3.sql
@@ -1,3 +1,66 @@
+-- check3.sql
+
+DROP FUNCTION IF EXISTS tmp_kana_to_romaji(text);
+DROP TABLE IF EXISTS tmp_manufacturer_normalized, tmp_katakana_romaji_map, tmp_manufacturer_romaji;
+
+CREATE TEMP TABLE tmp_manufacturer_normalized AS
+SELECT
+    manufacturer_name AS original_name,
+    COUNT(*) AS device_count,
+    UPPER(TRIM(REGEXP_REPLACE(
+        TRANSLATE(
+            manufacturer_name,
+            'ｦｧｨｩｪｫｬｭｮｯｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝ',
+            'ヲァィゥェォャュョッアイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワン'
+        ),
+        '(株式会社|有限会社|㈱|Co\.,?\s*Ltd\.?|Corp\.?|Corporation|Inc\.?|K\.K\.?)',
+        '', 'gi'
+    ))) AS normalized_name
+FROM cur.medical_device_ledger
+WHERE manufacturer_name IS NOT NULL
+GROUP BY manufacturer_name;
+
+CREATE TEMP TABLE tmp_katakana_romaji_map AS
+SELECT * FROM (VALUES
+    ('ｷｬ','kya'),('ｷｭ','kyu'),('ｷｮ','kyo'),('ｷﾞｬ','gya'),('ｷﾞｭ','gyu'),('ｷﾞｮ','gyo'),
+    ('ｼｬ','sha'),('ｼｭ','shu'),('ｼｮ','sho'),('ｼﾞｬ','ja'),('ｼﾞｭ','ju'),('ｼﾞｮ','jo'),
+    ('ﾁｬ','cha'),('ﾁｭ','chu'),('ﾁｮ','cho'),('ﾆｬ','nya'),('ﾆｭ','nyu'),('ﾆｮ','nyo'),
+    ('ﾋｬ','hya'),('ﾋｭ','hyu'),('ﾋｮ','hyo'),('ﾋﾞｬ','bya'),('ﾋﾞｭ','byu'),('ﾋﾞｮ','byo'),
+    ('ﾋﾟｬ','pya'),('ﾋﾟｭ','pyu'),('ﾋﾟｮ','pyo'),('ﾐｬ','mya'),('ﾐｭ','myu'),('ﾐｮ','myo'),
+    ('ﾘｬ','rya'),('ﾘｭ','ryu'),('ﾘｮ','ryo'),
+    ('ｶﾞ','ga'),('ｷﾞ','gi'),('ｸﾞ','gu'),('ｹﾞ','ge'),('ｺﾞ','go'),
+    ('ｻﾞ','za'),('ｼﾞ','ji'),('ｽﾞ','zu'),('ｾﾞ','ze'),('ｿﾞ','zo'),
+    ('ﾀﾞ','da'),('ﾁﾞ','ji'),('ﾂﾞ','zu'),('ﾃﾞ','de'),('ﾄﾞ','do'),
+    ('ﾊﾞ','ba'),('ﾋﾞ','bi'),('ﾌﾞ','bu'),('ﾍﾞ','be'),('ﾎﾞ','bo'),
+    ('ﾊﾟ','pa'),('ﾋﾟ','pi'),('ﾌﾟ','pu'),('ﾍﾟ','pe'),('ﾎﾟ','po'),
+    ('ｱ','a'),('ｲ','i'),('ｳ','u'),('ｴ','e'),('ｵ','o'),
+    ('ｶ','ka'),('ｷ','ki'),('ｸ','ku'),('ｹ','ke'),('ｺ','ko'),
+    ('ｻ','sa'),('ｼ','shi'),('ｽ','su'),('ｾ','se'),('ｿ','so'),
+    ('ﾀ','ta'),('ﾁ','chi'),('ﾂ','tsu'),('ﾃ','te'),('ﾄ','to'),
+    ('ﾅ','na'),('ﾆ','ni'),('ﾇ','nu'),('ﾈ','ne'),('ﾉ','no'),
+    ('ﾊ','ha'),('ﾋ','hi'),('ﾌ','fu'),('ﾍ','he'),('ﾎ','ho'),
+    ('ﾏ','ma'),('ﾐ','mi'),('ﾑ','mu'),('ﾒ','me'),('ﾓ','mo'),
+    ('ﾔ','ya'),('ﾕ','yu'),('ﾖ','yo'),
+    ('ﾗ','ra'),('ﾘ','ri'),('ﾙ','ru'),('ﾚ','re'),('ﾛ','ro'),
+    ('ﾜ','wa'),('ｦ','wo'),('ﾝ','n'),
+    ('ｧ',''),('ｨ',''),('ｩ',''),('ｪ',''),('ｫ',''),('ｯ',''),
+    ('ｰ','')
+) AS t(kana, romaji);
+
+CREATE OR REPLACE FUNCTION tmp_kana_to_romaji(input text) RETURNS text AS $$
+DECLARE
+    result text := input;
+    r RECORD;
+BEGIN
+    FOR r IN SELECT kana, romaji FROM tmp_katakana_romaji_map ORDER BY LENGTH(kana) DESC LOOP
+        result := REPLACE(result, r.kana, r.romaji);
+    END LOOP;
+    RETURN result;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Main output: all 179 manufacturers, Latin as-is / kana+kanji romanized,
+-- sorted by phonetic key so similar-sounding rows land near each other.
 SELECT
     m.original_name,
     m.device_count,
