e899b4bef2a47d0e83dfa4ef0949dc5b3d105705
verojnnd <y03372@yagami.co.jp>
2026-08-28 11:07:09 +0900

    Create null.sql

---
+++ null.sql
@@ -0,0 +1,38 @@
+SELECT
+    COUNT(*) AS total_rows,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE delivery_date IS NULL) / COUNT(*), 1) AS delivery_date_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE purchase_date IS NULL) / COUNT(*), 1) AS purchase_date_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE disposal_date IS NULL) / COUNT(*), 1) AS disposal_date_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE category_minor IS NULL) / COUNT(*), 1) AS category_minor_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE category_middle IS NULL) / COUNT(*), 1) AS category_middle_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE category_major IS NULL) / COUNT(*), 1) AS category_major_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE device_number IS NULL) / COUNT(*), 1) AS device_number_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE client_device_number IS NULL) / COUNT(*), 1) AS client_device_number_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE operation_start_date IS NULL) / COUNT(*), 1) AS operation_start_date_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE manufacturer_name IS NULL) / COUNT(*), 1) AS manufacturer_name_null_pct
+FROM cur.medical_device_ledger;
+
+SELECT
+    COUNT(*) AS total_rows,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE trouble_date IS NULL) / COUNT(*), 1) AS trouble_date_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE completion_date IS NULL) / COUNT(*), 1) AS completion_date_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE calculated_completion_date IS NULL) / COUNT(*), 1) AS calc_completion_date_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE downtime_hours IS NULL) / COUNT(*), 1) AS downtime_hours_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE calculated_downtime_hours IS NULL) / COUNT(*), 1) AS calc_downtime_hours_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE failure_reason IS NULL) / COUNT(*), 1) AS failure_reason_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE medical_device_ledger_id IS NULL) / COUNT(*), 1) AS unlinked_to_ledger_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE manufacturer_name IS NULL) / COUNT(*), 1) AS manufacturer_name_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE category_minor IS NULL) / COUNT(*), 1) AS category_minor_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE category_middle IS NULL) / COUNT(*), 1) AS category_middle_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE category_major IS NULL) / COUNT(*), 1) AS category_major_null_pct
+FROM cur.medical_device_repair_history;
+
+SELECT
+    COUNT(*) AS total_rows,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE rental_start_date IS NULL) / COUNT(*), 1) AS rental_start_date_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE return_date IS NULL) / COUNT(*), 1) AS return_date_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE calculated_return_date IS NULL) / COUNT(*), 1) AS calc_return_date_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE recipient_department IS NULL) / COUNT(*), 1) AS recipient_department_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE rental_duration_hours IS NULL) / COUNT(*), 1) AS rental_duration_hours_null_pct,
+    ROUND(100.0 * COUNT(*) FILTER (WHERE is_returned = false) / COUNT(*), 1) AS not_yet_returned_pct
+FROM cur.medical_device_rental_history;
